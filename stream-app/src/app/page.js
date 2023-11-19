aa1e65ea458b270616283299c4357abe7b59f5b8
Sagar ghare <sagar.ghare@acc.ltd>
2023-11-19 11:44:44 +0530

    stream app new project initiated

---
+++ stream-app/src/app/page.js
@@ -0,0 +1,10 @@
+const Page = () => {
+  const design = (
+    <>
+      <h1 className="text-5xl text-red-400 hover:text-red-700 hover:uppercase">welcome to the Page !</h1>
+    </>
+  );
+  return design;
+}
+
+export default Page;
