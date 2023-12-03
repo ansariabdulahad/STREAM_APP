f9cc93993fccce2f06e5432c8da6e0b947ada59d
Sagar ghare <sagar.ghare@acc.ltd>
2023-12-03 17:53:13 +0530

    animated useSpring code

--- stream-app/src/app/page.js
+++ stream-app/src/app/page.js
@@ -1,51 +1,63 @@
 'use client';
 
 import { useState } from "react";
-import { Button, Card, Expand, Fade, Flip, Icon, IconButton, Navbar, PatternOne, PatternTwo, Zoom } from "../../Tailwind";
+import { useSpring, animated } from "@react-spring/web";
+import { Button } from "../../Tailwind";
 
 const Page = () => {
 
-  const menus = {
-    brand: 'Just For Code',
-    link: [
-      {
-        label: 'Home',
-        href: '/'
-      },
-      {
-        label: 'About',
-        href: '/about'
-      },
-      {
-        label: 'Images',
-        href: '/images'
-      },
-      {
-        label: 'Blog',
-        href: '/blog'
-      }
-    ]
-  }
+  const [pause, setPause] = useState(false);
 
-  const toolbars = [
-    {
-      label: <IconButton className="bg-indigo-100 text-indigo-600">home</IconButton>,
-      href: '/'
+  const move = useSpring({
+    pause: pause,
+    loop: {
+      reverse: true
     },
-    {
-      label: <IconButton className="bg-indigo-100 text-indigo-600">edit</IconButton>,
-      href: '/'
+    // delay: 2000,
+    from: {
+      color: 'red',
+      top: 0,
+      left: 0
+    },
+    to: {
+      color: 'green',
+      top: 0,
+      left: 200
+    },
+    config: {
+      duration: 3000
     }
-  ]
+  });
+
+  const { number } = useSpring({
+    pause: pause,
+    loop: {
+      reverse: true,
+    },
+    delay: 2000,
+    from: {
+      number: 0
+    },
+    to: {
+      number: 1000
+    },
+    config: {
+      duration: 3000
+    }
+  })
 
   const design = (
     <>
-      <Navbar
-        menu={menus}
-        varient="three"
-        toolbar={toolbars}
-        theme="dark"
-      />
+      <animated.h1
+        style={move}
+        className={'text-9xl txt-red-500'}
+      >
+        {number.to((counter) => Math.floor(counter))}
+      </animated.h1>
+      <Button
+        className="mt-96"
+        onClick={() => setPause(!pause)}
+      >Control</Button>
     </>
   );
   return design;
