ef5563a989f786c5090a2e45e832bc50993661f1
Sagar ghare <sagar.ghare@acc.ltd>
2023-12-04 13:09:57 +0530

    useDrag hook code compelted

--- stream-app/src/app/page.js
+++ stream-app/src/app/page.js
@@ -3,8 +3,20 @@
 import { useState } from "react";
 import { useSpring, animated } from "@react-spring/web";
 import { Button } from "../../Tailwind";
+import { useDrag } from "react-use-gesture";
 
 const Page = () => {
+
+  const handleDrag = ({ offset }) => {
+    const left = offset[0];
+    const top = offset[1];
+    api.start({
+      x: left,
+      y: top
+    });
+  }
+
+  const gesture = useDrag(handleDrag);
 
   const [{ x, y }, api] = useSpring(() => ({
     x: 0,
@@ -22,6 +34,7 @@ const Page = () => {
   const design = (
     <>
       <animated.div
+        {...gesture()}
         style={{
           width: '200px',
           height: '200px',
@@ -32,6 +45,7 @@ const Page = () => {
       >
       </animated.div>
       <Button
+        {...gesture()}
         onClick={handleSpring}
         theme="secondary"
         className="mt-96">Move</Button>
